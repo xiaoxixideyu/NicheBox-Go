94a683ab4834fadbf55584c2a72682b304213158
xiaoxixideyu <yu153957@protonmail.com>
2024-01-07 00:33:38 +0800

    feat: user rpc service add mysql configuration

--- src/service/user/rpc/internal/config/config.go
+++ src/service/user/rpc/internal/config/config.go
@@ -4,4 +4,14 @@ import "github.com/zeromicro/go-zero/zrpc"
 
 type Config struct {
 	zrpc.RpcServerConf
+	Mysql struct {
+		DataBase        string
+		Username        string
+		Password        string
+		Host            string
+		Port            string
+		MaxIdleConns    int
+		MaxOpenConns    int
+		ConnMaxLifeTime int
+	}
 }
--- src/service/user/rpc/internal/svc/servicecontext.go
+++ src/service/user/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,25 @@
 package svc
 
-import "nichebox/service/user/rpc/internal/config"
+import (
+	"log"
+	"nichebox/service/user/model"
+	"nichebox/service/user/model/mysql"
+	"nichebox/service/user/rpc/internal/config"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config        config.Config
+	UserInterface model.UserInterface
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	userInterface, err := mysql.NewMysqlInterface(c.Mysql.DataBase, c.Mysql.Username, c.Mysql.Password, c.Mysql.Host, c.Mysql.Port, c.Mysql.MaxIdleConns, c.Mysql.MaxOpenConns, c.Mysql.ConnMaxLifeTime)
+	if err != nil {
+		log.Printf("failed to create user interface, err: %v\n", err)
+		return nil
+	}
 	return &ServiceContext{
-		Config: c,
+		Config:        c,
+		UserInterface: userInterface,
 	}
 }
