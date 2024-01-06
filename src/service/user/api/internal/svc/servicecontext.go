4875ad0014d0a9598f265917bb39840f0661559e
xiaoxixideyu <yu153957@protonmail.com>
2024-01-07 00:32:42 +0800

    feat:user api service adds user rpc dependency

--- src/service/user/api/internal/svc/servicecontext.go
+++ src/service/user/api/internal/svc/servicecontext.go
@@ -2,14 +2,19 @@ package svc
 
 import (
 	"nichebox/service/user/api/internal/config"
+	"nichebox/service/user/rpc/userclient"
+
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config  config.Config
+	UserRpc userclient.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:  c,
+		UserRpc: userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
 	}
 }
